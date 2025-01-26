f580aa21c64c5522967e4f31783590ff887b4f09
tangcongtong <tangcongtong271201@gmail.com>
2025-01-26 15:02:50 +0700

    Add project files.

---
+++ RateLimit/Program.cs
@@ -0,0 +1,44 @@
+﻿
+using AspNetCoreRateLimit;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+//IP Rate Limiting
+////Dòng này ánh xạ với IpRateLimiting trong file appsettings.json sang IpRateLimitOptions
+builder.Services.Configure<IpRateLimitOptions>(builder.Configuration.GetSection("IpRateLimiting"));
+////Dòng này ?? thêm cache, l?u tr? b? ??m và quy t?c th?i gian config
+builder.Services.AddMemoryCache();
+builder.Services.AddInMemoryRateLimiting();
+builder.Services.AddSingleton<IRateLimitConfiguration, RateLimitConfiguration>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+    app.UseCors(option =>
+                  option.AllowAnyOrigin()
+                        .AllowAnyMethod()
+                        .AllowAnyHeader());
+}
+else
+{
+    app.UseCors();
+}
+app.UseIpRateLimiting();
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
